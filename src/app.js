973c92ab08d9eb3ac219944a813c9caea1a03cf4
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-28 17:08:50 +0530

    comment

--- src/app.js
+++ src/app.js
@@ -5,3 +5,4 @@ const app=express();
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
 });
+// Our server is listening at port 3000. But, we're not handling incoming requests
