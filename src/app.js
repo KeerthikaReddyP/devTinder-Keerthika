7c4fbdf6e644beb81a087890e17a9bad1ad99475
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-28 17:10:52 +0530

    Request Handler

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express=require('express');
 
 const app=express();
 
+app.use((req,res)=>{
+    res.send("Hello from the server!");
+});
+
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
 });
-// Our server is listening at port 3000. But, we're not handling incoming requests
