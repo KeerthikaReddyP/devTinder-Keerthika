ad2b3f192cf9a3639d030171392dc218d3ba0856
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-28 17:14:25 +0530

    Handling different requests

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 });
 
+app.use("/hello",(req,res)=>{
+    res.send("Hello hello hello!");
+});
+
+app.use("/",(req,res)=>{
+    res.send("Hi theree...");
+});
+
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
 });
