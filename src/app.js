887b6ffae16a29f4ba2268ec48389c0fb52683d0
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-30 19:33:48 +0530

    Keeping the route / on top of other

--- src/app.js
+++ src/app.js
@@ -2,16 +2,18 @@ const express=require('express');
 
 const app=express();
 
+// Keeping this route (/) on top of other
+// This will give the same response (Hi there..) for all the routes like /hello, /test, /xyz(which we didn't even define)
+app.use("/",(req,res)=>{
+    res.send("Hi thereee...");
+});
+
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 });
 
 app.use("/hello",(req,res)=>{
     res.send("Hello hello hello!");
-});
-
-app.use("/",(req,res)=>{
-    res.send("Hi thereee...");
 });
 
 app.listen(3000,()=>{
