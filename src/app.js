1faa585f2a6dfef75a1b161b682c6bd19976873e
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-01-31 19:57:49 +0530

    something

--- src/app.js
+++ src/app.js
@@ -4,13 +4,14 @@ const app=express();
 
 // Keeping this route (/) on top of other
 // This will give the same response (Hi there..) for all the routes like /hello, /test, /xyz(which we didn't even define)
-app.use("/",(req,res)=>{
-    res.send("Hi thereee...");
-});
+// app.use("/",(req,res)=>{
+//     res.send("Hi thereee...");
+// });
 
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
 });
+// It'll give the same response for '/test' and '/test/anything'
 
 app.use("/hello",(req,res)=>{
     res.send("Hello hello hello!");
