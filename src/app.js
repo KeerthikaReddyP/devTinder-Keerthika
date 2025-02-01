8f14354d7a4a6299c8946dc1316490d25cbf60c4
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-01 16:13:52 +0530

    Order of the routes matter

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const app=express();
 
 // Keeping this route (/) on top of other
 // This will give the same response (Hi there..) for all the routes like /hello, /test, /xyz(which we didn't even define)
-// app.use("/",(req,res)=>{
-//     res.send("Hi thereee...");
-// });
+app.use("/",(req,res)=>{
+    res.send("Hi thereee...");
+});
 
 app.use("/test",(req,res)=>{
     res.send("Hello from the server!");
