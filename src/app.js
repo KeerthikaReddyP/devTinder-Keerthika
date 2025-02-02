512c29cfded8e162676ed5d2dc93aec1f576d5e8
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-02 20:07:20 +0530

    removed comments

--- src/app.js
+++ src/app.js
@@ -2,23 +2,10 @@ const express=require('express');
 
 const app=express();
 
-app.use("/user",(req,res)=>{
-    res.send("HAHAHAHHA");
-}); // Now, this will  give HAHAHA, bcz order matters.(It is n the top)
-
-// This will match or handle only GET call to /user
+// This will match for all the GET calls
+// with routes /user, /user/xyz, /user/anything, /user/1
 app.get("/user", (req,res)=>{
     res.send({firstname:"Keerthika",lastname:"Panchika"});
-});
-
-app.post("/user",(req,res)=>{
-    //Code to save data to the database
-    res.send("Data saved successfully to the database!");
-});
-
-// This will match all the HTTP methods
-app.use("/test",(req,res)=>{
-    res.send("Hello from the server!");
 });
 
 app.listen(3000,()=>{
