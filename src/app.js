54ea68e84b14a80abaaf52e441d5b3e5fed05c99
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 16:36:48 +0530

    What if we dont send any response from the server?

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express=require('express');
 
 const app=express();
 
+app.use("/user",(req,res)=>{
+    // res.send("jhg");
+    //What if we dont send any response from the server?
+    // It just says sending request.. (like infinite loop)
+});
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
