0db6308f54e78776370b719a31aee238749be989
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 18:28:07 +0530

    nothing

--- src/app.js
+++ src/app.js
@@ -1,25 +1,32 @@
-const express=require('express');
+const express = require("express");
 
-const app=express();
+const app = express();
 
-app.use("/user",(req,res,next)=>{
+app.use(
+  "/user",
+  (req, res, next) => {
     console.log("Handling the route user");
-    next();
     res.send("Response");
-}, (req,res)=>{
+    next();
+  },
+  (req, res, next) => {
     console.log("Handling the route user 2");
     res.send("Response-2");
-}, (req,res)=>{
+  },
+  (req, res) => {
     console.log("Handling the route user 3");
     res.send("Response-3");
-}, (req,res)=>{
+  },
+  (req, res) => {
     console.log("Handling the route user 4");
     res.send("Response-4");
-}, (req,res)=>{
+  },
+  (req, res) => {
     console.log("Handling the route user 5");
     res.send("Response-5");
-});
+  }
+);
 
-app.listen(3000,()=>{
-    console.log("Server is successfully listening on port 3000...");
+app.listen(3000, () => {
+  console.log("Server is successfully listening on port 3000...");
 });
