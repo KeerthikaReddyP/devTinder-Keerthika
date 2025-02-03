10157ff17d1ac1482a15de15561102afd45ec678
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 22:13:41 +0530

    We can also create one more route handler with same route

--- src/app.js
+++ src/app.js
@@ -2,35 +2,16 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/route", rh1,rh2,rh3,rh4,....)
-// app.use("/route", [rh1,rh2,rh3,rh4,....])
-// app.use("/route", [rh1,rh2],rh3,rh4,....)
-// app.use("/route", rh1,[rh2,rh3,rh4],rh5,....)
 
-app.use(
-  "/user",
-  [(req, res, next) => {
-    console.log("Handling the route user");
-    res.send("Response");
-    next();
-  },
-  (req, res, next) => {
-    console.log("Handling the route user 2");
-    res.send("Response-2");
-  }],
-  (req, res) => {
-    console.log("Handling the route user 3");
-    res.send("Response-3");
-  },
-  (req, res) => {
-    console.log("Handling the route user 4");
-    res.send("Response-4");
-  },
-  (req, res) => {
-    console.log("Handling the route user 5");
-    res.send("Response-5");
-  }
-);
+app.get("/user", (req, res, next) => {
+  console.log("Handling the route user");
+  next();
+});
+app.get("/user", (req, res) => {
+  console.log("Handling the route user 2");
+  res.send("Response-2");
+});
+
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
