f36de712db22840d12fc2acb047e17f84af70520
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 17:04:45 +0530

    Two route handlers

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ const app=express();
 
 app.use("/user",(req,res)=>{
     console.log("Handling the route user");
-    res.send("Response");
+    // res.send("Response");
 }, (req,res)=>{
     console.log("Handling the route user 2");
     res.send("Response-2");
 });
+// This will give "Response" (First route handler)
+
+// But what if we comment res.send on first route handler?
+// Will it go to 2nd?
+// No. It will give nothing as response.
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
