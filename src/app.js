80a07bbdb75133d9af2e82a23041e12e871c3c76
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 18:20:56 +0530

    Removed comments

--- src/app.js
+++ src/app.js
@@ -19,22 +19,6 @@ app.use("/user",(req,res,next)=>{
     console.log("Handling the route user 5");
     res.send("Response-5");
 });
-// This will give "Response" (First route handler)
-
-// But what if we comment res.send on first route handler?
-// Will it go to 2nd?
-// No. It will give nothing as response.
-
-// How to go to 2nd route handler then??
-// next()
-
-// What if we keep res.send in first route handler?
-// It will give first response in postman.
-// But also an error in the console. Bcz, we've already sent a response to the client.
-
-// What if next() is before res.send?
-// This will give Response-2 in postman.
-// Also, two console logs and an error in the console.
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
