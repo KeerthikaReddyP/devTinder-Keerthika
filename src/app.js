d271f0b5086a93d4cd95367e0d422c1ee0dff22b
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 17:16:43 +0530

    What if next() is before res.send?

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const app=express();
 
 app.use("/user",(req,res,next)=>{
     console.log("Handling the route user");
-    res.send("Response");
     next();
+    res.send("Response");
 }, (req,res)=>{
     console.log("Handling the route user 2");
     res.send("Response-2");
@@ -22,6 +22,10 @@ app.use("/user",(req,res,next)=>{
 // What if we keep res.send in first route handler?
 // It will give first response in postman.
 // But also an error in the console. Bcz, we've already sent a response to the client.
+
+// What if next() is before res.send?
+// This will give Response-2 in postman.
+// Also, two console logs and an error in the console.
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
