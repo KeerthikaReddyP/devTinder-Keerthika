2640a4e924dd47a090014101b217df5e4f11fe9c
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-03 16:34:03 +0530

    cleaned the code

--- src/app.js
+++ src/app.js
@@ -2,11 +2,6 @@ const express=require('express');
 
 const app=express();
 
-// ab (anything) cd
-// works for /abcd, /abefhsdcscd, /abanythingcd
-app.get("/ab*cd", (req,res)=>{
-    res.send({firstname:"Keerthika",lastname:"Panchika"});
-});
 
 app.listen(3000,()=>{
     console.log("Server is successfully listening on port 3000...");
