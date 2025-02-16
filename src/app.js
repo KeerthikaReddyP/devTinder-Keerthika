2e43d25a825b8d411a037f8bb3cc9165ba225e69
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-02-16 20:03:02 +0530

    Random nothing

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ app.get("/user", (req, res) => {
   console.log("Handling the route user 2");
   res.send("Response-2");
 });
-
+app.get("/test",(req,res,next)=>{
+  console.log("Handling the test route");
+  next();
+});
+app.get("/test",(req,res)=>{
+  console.log("Handling the test route -2 ");
+  res.send("Response sent for test");
+});
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 3000...");
